5f9140279d54c0e6d6a4ee14d6afce7b2031a7df
Mateus Valgueiro <mvalgueiro@apple.com>
2019-05-20 21:34:57 -0300

    Make typeEnum more useful

--- CymbolCheckerVisitor.py
+++ CymbolCheckerVisitor.py
@@ -1,35 +1,25 @@
 from antlr4 import *
 from autogen.CymbolParser import CymbolParser 
 from autogen.CymbolVisitor import CymbolVisitor
+from enum import Enum
 
-class TypeEnum:
+class TypeEnum(Enum):
 	INT = "int"#, llvm_type: "i32"}
 	FLOAT ="float" # llvm_type: "float"}
 	BOOLEAN = "boolean" #llvm_type: "i8"}
 
-
-class Type:
-	def __init__(self, name):
-		self.name = name
-
-		llvm_type = ""
-		if self.name == TypeEnum.INT:
-			llvm_type = "i32"
-		elif self.name == TypeEnum.FLOAT:
-			llvm_type = "float"
-		elif self.name == TypeEnum.BOOLEAN:
-			llvm_type = "i8"
-
-		self.llvm_type = llvm_type
-
 	def get_llvm_type(self):
-		return self.llvm_type
-	
+		llvm_type = ""
+		if self.value == self.INT.value:
+			llvm_type = "i32"
+		elif self.value == self.FLOAT.value:
+			llvm_type = "float"
+		elif self.value == self.BOOLEAN.value:
+			llvm_type = "i8"
+		return llvm_type
+
 	def __str__(self):
 		return self.name + "   ->  " + self.llvm_type
-
-
-
 
 class CymbolCheckerVisitor(CymbolVisitor):
 	def visitFiile(self, ctx:CymbolParser.FiileContext):
@@ -45,7 +35,7 @@ class CymbolCheckerVisitor(CymbolVisitor):
 			file.write(output)
 
 	def visitFuncDecl(self, ctx:CymbolParser.FuncDeclContext):
-		tyype = Type(ctx.tyype().getText())
+		tyype = TypeEnum(ctx.tyype().getText())
 		name = ctx.ID().getText()
 
 		block = ""
@@ -64,7 +54,7 @@ class CymbolCheckerVisitor(CymbolVisitor):
 
 
 	def visitVarDecl(self, ctx:CymbolParser.VarDeclContext):
-		tyype = Type(ctx.tyype().getText())
+		tyype = TypeEnum(ctx.tyype().getText())
 		name = ctx.ID().getText()
 		out = tyype.get_llvm_type() + " " + name + ";\n"
 		return out
@@ -80,5 +70,7 @@ class CymbolCheckerVisitor(CymbolVisitor):
 		return out
 
 	def visitIntExpr(self, ctx:CymbolParser.IntExprContext):
+		t = TypeEnum.INT.get_llvm_type()
+		print(t)
 		return 'i32 ' + ctx.getText()
     
