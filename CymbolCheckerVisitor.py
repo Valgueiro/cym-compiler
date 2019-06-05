da9e235a68d67d4ebee1fc4a607e882f9c4565cb
Mateus Valgueiro <mvalgueiro@apple.com>
2019-06-05 15:31:43 -0300

    Add EqualNotEqualExpr

--- CymbolCheckerVisitor.py
+++ CymbolCheckerVisitor.py
@@ -326,3 +326,28 @@ class CymbolCheckerVisitor(CymbolVisitor):
             out_reg = function_heaps[self.namefunc].get_new_register()
             out = f'%{out_reg} = {symbol} {tyype} {register_1}, {register_2}'
             return Expr(TypeEnum.BOOLEAN, f'%{out_reg}', declarations=out, loaded= True, nmfnc=self.namefunc)
+    
+    def visitEqualNotEqualExpr(self, ctx: CymbolParser.EqualNotEqualExprContext):
+        expr_1 = self.visit(ctx.expr()[0])
+        expr_2 = self.visit(ctx.expr()[1])
+        
+        symbol = 'icmp '
+        if ctx.op.text == '==':
+            symbol += 'eq'
+        else:
+            symbol += 'ne'
+
+        if expr_1.type == expr_2.type:
+            tyype = expr_1.type
+
+            register_1 = expr_1.get_assigned_register()
+            if expr_1.declarations != "":
+                out += expr_1.declarations + '\n'
+
+            register_2 = expr_2.get_assigned_register()
+            if expr_2.declarations != "":
+                out += (expr_2.declarations + '\n')
+            
+            out_reg = function_heaps[self.namefunc].get_new_register()
+            out = f'%{out_reg} = {symbol} {tyype} {register_1}, {register_2}'
+            return Expr(TypeEnum.BOOLEAN, f'%{out_reg}', declarations=out, loaded= True, nmfnc=self.namefunc)
